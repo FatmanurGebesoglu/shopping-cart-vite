7bc426677d644a9b77dd1ea7067cc5a125a0991c
Fatmanur Gebeşoğlu <76436684+FatmanurGebesoglu@users.noreply.github.com>
2024-02-02 00:33:07 +0300

    Products component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header.jsx"
+import Products from "./components/Products.jsx"
 
 function App() {
 
   return (
     <div className='container mx-auto p-4'>
       <Header />
+      <Products />
     </div>
   )
 }
