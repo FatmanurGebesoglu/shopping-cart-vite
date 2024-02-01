13a65409f9eb2560b47837b6654aeb2b408724b0
Fatmanur Gebeşoğlu <76436684+FatmanurGebesoglu@users.noreply.github.com>
2024-02-02 00:11:50 +0300

    header component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import Header from "./components/Header"
+import Header from "./components/Header.jsx"
 
 function App() {
 
   return (
-    <div className='container mx-auto'>
+    <div className='container mx-auto p-4'>
       <Header />
     </div>
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { FiShoppingCart } from "react-icons/fi";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="flex justify-between items-center mb-6">
+      <h1 className="text-3xl font-semibold">
+        React ve Tailwind CSS Sepet Uygulaması
+      </h1>
+      <div className="relative"> 
+        <FiShoppingCart className="text-2xl" />
+        <span className="bg-red-500 text-white w-5 h-5 flex justify-center items-center rounded-full absolute -top-2 -right-4 text-xs">0</span>
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
