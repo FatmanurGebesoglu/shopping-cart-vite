54600355ebf767a6fe40eeb1eed1b34d6fbd3c36
Fatmanur Gebeşoğlu <76436684+FatmanurGebesoglu@users.noreply.github.com>
2024-02-02 23:43:55 +0300

    productData created for fake product info

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-const ProductItem = () => {
+const ProductItem = ({product}) => {
   return <div className="border p-4 m-2 rounded-lg shadow-lg">
-    <img  className="w-full h-48 object-cover rounded-t-lg" src="https://placehold.jp/150x150.png" alt="product"/>   
+    <img  className="w-full h-48 object-cover rounded-t-lg" src={product.image} alt="product"/>   
     <div className="p-4">
-        <h2 className="text-xl font-semibold">Ürün 1</h2>
-        <p className="text-gray-500 my-2">100₺</p>
+        <h2 className="text-xl font-semibold">{product.name}</h2>
+        <p className="text-gray-500 my-2">{product.price}</p>
         <button className="bg-blue-500 text-white px-4 py-[8px] rounded hover:bg-blue-600 w-full ">Sepete Ekle</button>
     </div>
   </div>;
