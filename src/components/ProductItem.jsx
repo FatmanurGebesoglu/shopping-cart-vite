d95f3ec1b8f7507c3783d576249b2a24f99bf7df
Fatmanur Gebeşoğlu <76436684+FatmanurGebesoglu@users.noreply.github.com>
2024-02-03 01:47:16 +0300

    product item component edited

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
-const ProductItem = ({ product,  setCart }) => {
+const ProductItem = ({ product, cart,  setCart }) => {
+  const findProduct = cart.find((item) => item.id === product.id);
   const addToCart = (product) => {
     setCart((prevState) => [...prevState, product])
   };
@@ -16,8 +17,9 @@ const ProductItem = ({ product,  setCart }) => {
         <h2 className="text-xl font-semibold">{product.name}</h2>
         <p className="text-gray-500 my-2">{product.price}</p>
         <button
-          className="bg-blue-500 text-white px-4 py-[8px] rounded hover:bg-blue-600 w-full "
+          className={`bg-blue-500 text-white px-4 py-[8px] rounded hover:bg-blue-600 w-full ${findProduct && "opacity-50 cursor-no-drop"}`}
           onClick={() => addToCart(product)}
+          disabled={findProduct}
         >
           Sepete Ekle
         </button>
