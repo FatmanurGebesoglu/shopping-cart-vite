ed484cbc79f887e0083a2edb1cce7f578ab3c9a8
Fatmanur Gebeşoğlu <76436684+FatmanurGebesoglu@users.noreply.github.com>
2024-02-03 01:30:37 +0300

    cart total added

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 
 const Cart = ({ cart, emptyCart }) => {
+   
+    const total = cart.reduce((acc, item) => acc + item.price, 0)
+    
   if (cart.length === 0) return;
   return (
     <div className="border ml-auto w-72 p-4 mt-2 rounded-lg shadow-lg">
@@ -14,7 +17,7 @@ const Cart = ({ cart, emptyCart }) => {
           ))}
         </ul>
         <hr className="my-4" />
-        <p className="font-semibold text-xl">Toplam: 100₺</p>
+        <p className="font-semibold text-xl">Toplam: {total} ₺</p>
         <button
           className="bg-red-500 px-4 py-2 rounded w-full hover:bg-red-600 transition-all mt-4"
           onClick={emptyCart}
