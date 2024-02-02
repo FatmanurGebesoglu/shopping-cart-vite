fadda58eabc1eb4fe2ea6e7ad2fedb8d1b21e2ec
Fatmanur Gebeşoğlu <76436684+FatmanurGebesoglu@users.noreply.github.com>
2024-02-03 01:24:42 +0300

    cart props added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
+import { useState } from "react"
 import Cart from "./components/Cart.jsx"
 import Header from "./components/Header.jsx"
 import Products from "./components/Products.jsx"
 
 function App() {
 
+  const [cart, setCart] = useState([]);
+
+  const emptyCart = () => {
+    setCart([]);
+  };
+
   return (
     <div className='container mx-auto p-4'>
-      <Header />
-      <Products />
-      <Cart />
+      <Header cart={cart}/>
+      <Products cart={cart} setCart={setCart}/>
+      <Cart cart={cart} setCart={setCart} emptyCart={emptyCart}/>
     </div>
   )
 }
 
-export default App
+export default App;
--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,21 +1,29 @@
-import React from 'react'
+import React from "react";
 
-const Cart = () => {
+const Cart = ({ cart, emptyCart }) => {
+  if (cart.length === 0) return;
   return (
-    <div className='border ml-auto w-72 p-4 mt-2 rounded-lg shadow-lg'>
-        <h2 className='text-2xl font-semibold mb-4 '>
-            <ul>
-                <li className='mt-2 flex justify-between'>
-                    <span>Ürün 1</span>
-                    <span>100₺</span>
-                </li>
-            </ul>
-            <hr className='my-4'/>
-            <p className='font-semibold text-xl'>Toplam: 100₺</p>
-            <button className='bg-red-500 px-4 py-2 rounded w-full hover:bg-red-600 transition-all mt-4'>Sepeti Boşalt</button>
-        </h2>
+    <div className="border ml-auto w-72 p-4 mt-2 rounded-lg shadow-lg">
+      <h2 className="text-2xl font-semibold mb-4 ">
+        <ul>
+          {cart.map((item) => (
+            <li className="mt-2 flex justify-between">
+              <span>{item.name}</span>
+              <span>{item.price}₺</span>
+            </li>
+          ))}
+        </ul>
+        <hr className="my-4" />
+        <p className="font-semibold text-xl">Toplam: 100₺</p>
+        <button
+          className="bg-red-500 px-4 py-2 rounded w-full hover:bg-red-600 transition-all mt-4"
+          onClick={emptyCart}
+        >
+          Sepeti Boşalt
+        </button>
+      </h2>
     </div>
-  )
-}
+  );
+};
 
-export default Cart
+export default Cart;
