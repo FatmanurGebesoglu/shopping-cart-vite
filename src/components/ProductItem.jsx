92153b55280d4e49dd74e5fa5a76fe9e60717243
Fatmanur Gebeşoğlu <76436684+FatmanurGebesoglu@users.noreply.github.com>
2024-02-02 22:42:53 +0300

    added css for productitem component

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -3,10 +3,10 @@ import React from "react";
 const ProductItem = () => {
   return <div className="border p-4 m-2 rounded-lg shadow-lg">
     <img  className="w-full h-48 object-cover rounded-t-lg" src="https://placehold.jp/150x150.png" alt="product"/>   
-    <div>
-        <h2>Ürün 1</h2>
-        <p>100₺</p>
-        <button>Sepete Ekle</button>
+    <div className="p-4">
+        <h2 className="text-xl font-semibold">Ürün 1</h2>
+        <p className="text-gray-500 my-2">100₺</p>
+        <button className="bg-blue-500 text-white px-4 py-[8px] rounded hover:bg-blue-600 w-full ">Sepete Ekle</button>
     </div>
   </div>;
 };
